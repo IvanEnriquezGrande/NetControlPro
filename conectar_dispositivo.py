c3c34c939aae61550f0dff2d575e3a39c5a12ae4
Ivan Enriquez Grande <ivan.enriquez.grande@gmail.com>
2023-11-11 18:53:32 -0600

    CONFIG METHODS ADDED TO conectar'dispositivo.py (activate_cdp, disable_cdp, activate_root_bridge_primary, get_device_vlans)

--- conectar_dispositivo.py
+++ conectar_dispositivo.py
@@ -1,26 +1,19 @@
 from netmiko import ConnectHandler
-#from app import conectar_db
-import mysql.connector
 
-"""
-def insertar_tipo_dispositivo(dispositivo):
-    mydb = conectar_db()
-    cursor = mydb.cursor()
+#Method that connnects to a specific device
+def device_connection(ip, username, password):
+    switch = {
+        'host': ip,
+        'username': username,
+        'password': password,
+        'secret': password,
+        'device_type': 'cisco_ios',
+        'read_timeout_override': 90
+    }
+    conexion = ConnectHandler(**switch)
+    return conexion
 
-    cursor.execute("INSERT INTO devices (device_type) VALUES (dispositivo);")
-    mydb.commit()
-    mydb.close()
-"""
-    
-def obtener_tipo_disp(dispositivo):
-    datos_dispositivo = dispositivo.get_facts()
-
-    if 'vlan_list' in datos_dispositivo:
-        return 'switch'
-    else:
-        return 'router'
-
-def obtener_tipo_dispositivo(dispositivo):
+def device_type(dispositivo):
     output = dispositivo.send_command('show vlan brief')
     print(output)
     
@@ -29,39 +22,7 @@ def obtener_tipo_dispositivo(dispositivo):
     else:
         return "switch"
     
-"""        
-#@app.route('/conexion-switch', methods=["GET", "POST"])
-def conexion_switch(ip, username, password):
-    switch = {
-        'ip': ip,
-        'username': username,
-        'passsword': password,
-        'device_type': 'cisco_ios',
-    }
-
-    conexion = ConnectHandler(**switch)
-    tipo_dispositivo = obtener_tipo_disp(conexion)
-    insertar_tipo_dispositivo(tipo_dispositivo)
-
-    return 0
-
-#@app.route('/conexion-router', methods=["GET", "POST"])
-def conexion_router(ip, username, password):
-    router = {
-        'ip': ip,
-        'username': username,
-        'passsword': password,
-        'device_type': 'cisco_ios',
-    }
-
-    conexion = ConnectHandler(**router)
-    tipo_dispositivo = obtener_tipo_disp(conexion)
-    insertar_tipo_dispositivo(tipo_dispositivo)
-
-    return 0
-"""
-    
-def conexion_equipo(ip, username, password):
+def get_device_type(ip, username, password):
     switch = {
         'host': ip,
         'username': username,
@@ -70,6 +31,30 @@ def conexion_equipo(ip, username, password):
     }
 
     conexion = ConnectHandler(**switch)
-    tipo_dispositivo = obtener_tipo_dispositivo(conexion)
-    print(tipo_dispositivo)
+    tipo_dispositivo = device_type(conexion)
     return tipo_dispositivo
+
+
+def get_device_vlans(equipo):
+    raw_vlans = equipo.send_command('show vlan brief')
+    return raw_vlans
+
+def modify_stp_value(device):
+    pass
+
+def activate_cdp(device):
+    commands = ['cdp run']
+    device.enable()
+    output = device.send_config_set(commands)
+    return output
+
+def disable_cdp(device):
+    commands = ['no cdp run']
+    device.enable()
+    output = device.send_config_set(commands)
+    return output
+
+def activate_root_bridge_primary(device):
+    commands = ['spanning-tree vlan 1 root primary']
+    device.enable()
+    device.send_config_set(commands)
